92ec1f007937c4d9b3bdb9fbdf26787bcad10a99
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2024-12-12 14:04:13 +0530

    developed the side nav for the app

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React , {useState} from "react";
 import logo from "./Assets/logo.svg";
 import { categoriesArray, navItems } from "../../Utils/utils";
-import { IoLocationSharp,} from "react-icons/io5";
+import { IoLocationSharp,IoCloseCircleOutline} from "react-icons/io5";
+import { HiOutlineMenu } from "react-icons/hi";
 
 const Navbar = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () =>{
+    setIsMenuOpen(!isMenuOpen) 
+    // document.body.style.overflow = "hidden" 
+  }
   return (
     <div className="w-full h-fit border-b">
-      <div className="max-w-screen-xl h-16 mx-auto flex items-center gap-8 px-3">
+      <div className="max-w-screen-xl h-[8vh] mx-auto flex items-center gap-8 px-3 relative">
         {/* logo section  */}
         <img className="w-32 cursor-pointer" src={logo} alt="" />
 
@@ -28,7 +36,7 @@ const Navbar = () => {
         </form>
 
         {/* location section  */}
-        <div className="flex items-baseline gap-2 cursor-pointer">
+        <div className="hidden lg:flex items-baseline gap-2 cursor-pointer">
           <p className="text-xl text-green-600">
             <IoLocationSharp />
           </p>
@@ -37,11 +45,21 @@ const Navbar = () => {
           
           {/* nav elements  */}
         {navItems.map((Element, id) => (
-          <div key={id} className="flex items-baseline gap-2 cursor-pointer">
+          <div key={id} className="hidden lg:flex items-baseline gap-2 cursor-pointer">
             <p className="text-2xl text-green-600">
               {<Element.icon/>}
             </p>
             <p className="font-semibold text-green-800">{Element.element}</p>
+          </div>
+        ))}
+
+        <p onClick={toggleMenu} className="text-2xl cursor-pointer absolute right-2 lg:hidden" >{isMenuOpen ? <IoCloseCircleOutline/> : <HiOutlineMenu/>}</p>
+      </div>
+      <div className={`absolute w-[90vw] h-[92vh]  py-4 bg-black text-white mx-auto px-3 transition-all duration-150 ease-in-out ${isMenuOpen ? "right-[-1%]" : "right-[-100%]"} `} >
+        {navItems.map((element , id) =>(
+          <div key={id} className="flex items-center gap-3 cursor-pointer leading-10" >
+            <p>{<element.icon/>}</p>
+            <p>{element.element}</p>
           </div>
         ))}
       </div>
